bd6e21803ef62ef2bac58dad0a055f725340f857
Boveloco <boveloco@gmail.com>
2016-09-27 15:29:25 -0300

    Arrumei a fisica, " o problema era o tamanho do vetor velocidade, estava muito pequeno e a forca adicionada era ínfima perto da massa que tinha colocado pra isso, reduzi a massa e coloquei um multiplicador para deixa-lo fluido. Coloquei as bordas. Coloquei o chao. Fiz com que mais de um projétil possa spawnar. TODO: Melhorar o sistema do slingshot para ficar mais direcional e criar o castelo.

--- BaseBox2D/src/ofApp.cpp
+++ BaseBox2D/src/ofApp.cpp
@@ -2,121 +2,157 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    
+    ofSetVerticalSync(true);
+    ofBackgroundHex(0xfdefc2);
+    ofSetLogLevel(OF_LOG_NOTICE);
+    ofDisableAntiAliasing();
+    
+    box2d.init();
+    box2d.setGravity(0, 15);
+    box2d.setFPS(60.0);
+    box2d.registerGrabbing();
+    
+    anchor.setup(box2d.getWorld(), 100, ofGetHeight() - ofGetHeight() / 3 - 25, 4);
+    anchor2.setup(box2d.getWorld(), 135, ofGetHeight() - ofGetHeight() / 3 + 35, 4);
+    
+    
+    //projetil
+    shared_ptr<ofxBox2dCircle> projectile = shared_ptr<ofxBox2dCircle>(new ofxBox2dCircle);
+    projectile.get()->setPhysics(30.0, 0.58, 0.3);
+    projectiles.push_back(projectile);
+    
+    circle.setPhysics(3.0, 0.58, 0.1);
+    circle.setup(box2d.getWorld(), 117,  ofGetHeight() - ofGetHeight() / 3 + 10 , 8);
+    
+    //joints
+    shared_ptr<ofxBox2dJoint> joint = shared_ptr<ofxBox2dJoint>(new ofxBox2dJoint);
+    joint.get()->setup(box2d.getWorld(), anchor.body, circle.body);
+    joints.push_back(joint);
 
-	ofSetVerticalSync(true);
-	ofBackgroundHex(0xfdefc2);
-	ofSetLogLevel(OF_LOG_NOTICE);
-	ofDisableAntiAliasing();
-
-	box2d.init();
-	box2d.setGravity(0, 1);
-	box2d.setFPS(60.0);
-	box2d.registerGrabbing();
-
-	anchor.setup(box2d.getWorld(), 100, ofGetHeight() - ofGetHeight() / 3, 4);
-	anchor2.setup(box2d.getWorld(), 100, ofGetHeight() - ofGetHeight() / 3 + 100, 4);
-
-	//projetil
-	projectile.setPhysics(300.0, 0.58, 0.1);
-
-	circle.setPhysics(3.0, 0.58, 0.1);
-	circle.setup(box2d.getWorld(), 100, ofGetHeight() - ofGetHeight() /3 + 50, 8);
-
-	shared_ptr<ofxBox2dJoint> joint = shared_ptr<ofxBox2dJoint>(new ofxBox2dJoint);
-	joint.get()->setup(box2d.getWorld(), anchor.body, circle.body);
-	joints.push_back(joint);
-
-
-	joint = shared_ptr<ofxBox2dJoint>(new ofxBox2dJoint);
-	joint.get()->setup(box2d.getWorld(), circle.body, anchor2.body);
-	joints.push_back(joint);
-
-
+    joint = shared_ptr<ofxBox2dJoint>(new ofxBox2dJoint);
+    joint.get()->setup(box2d.getWorld(), circle.body, anchor2.body);
+    joints.push_back(joint);
+    
+    //ground
+    ground.setup(box2d.getWorld(),ofRectangle(ofPoint(ofGetWidth() * 0.5, ofGetHeight()), ofGetWidth(), 100));
+    ground2.setup(box2d.getWorld(), ofRectangle(ofPoint(ofGetWidth() / 3 * 2, ofGetHeight() / 3 * 2), 500, 30));
+    wall1.setup(box2d.getWorld(), ofRectangle(ofPoint(ofGetWidth(), ofGetHeight() * 0.5), 30, ofGetHeight() ));
+    wall2.setup(box2d.getWorld(), ofRectangle(ofPoint(0, ofGetHeight() * 0.5), 30, ofGetHeight() ));
+    
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-	box2d.update();
-
-	if (drag) {
-		if (circle.getPosition().x >= 100) {
-			projectile.setup(box2d.getWorld(), circle.body->GetPosition().x + 50, circle.body->GetPosition().y + 20, 30);
-			drag = false;
-		}
-	}
-	else if (circle.getPosition().x <= 80) {
-		drag = !drag;
-	}
+    box2d.update();
+    
+    if (drag) {
+        if (circle.getPosition().x >= 110) {
+            addProjectile();
+            drag = false;
+        }
+    }
+    else if (circle.getPosition().x <= 100) {
+        drag = !drag;
+    }
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    
+    //grouds and walls
+    ground.draw();
+    ground2.draw();
+    wall1.draw();
+    wall2.draw();
+    
+    ofSetHexColor(0x0);
+    anchor.draw();
+    anchor2.draw();
+    
+    ofFill();
+    ofSetHexColor(0x0);
+    circle.draw();
+    for (int i = 0; i < projectiles.size(); i++){
+        ofSetHexColor(0x0);
+        projectiles[i].get()->draw();
+    }
+    
+    for (int i = 0; i < joints.size(); i++) {
+        ofSetHexColor(0x444342);
+        joints[i].get()->draw();
+    }
 
-	ofSetHexColor(0x0);
-	anchor.draw();
-	anchor2.draw();
-	
-	ofFill();
-	ofSetHexColor(0x0);
-	circle.draw();
-	projectile.draw();
-	for (int i = 0; i < joints.size(); i++) {
-		ofSetHexColor(0x444342);
-		joints[i].get()->draw();
-	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    if (key == 'n'){
+        addProjectile();
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
+    
+}
 
+void ofApp::addProjectile(){
+    float multiply = 120;
+    shared_ptr<ofxBox2dCircle> projectile = shared_ptr<ofxBox2dCircle>(new ofxBox2dCircle);
+    projectile.get()->setPhysics(3.0, 0.58, 1);
+    b2Vec2 vel = circle.body->GetLinearVelocity();
+    projectile.get()->setup(box2d.getWorld(), circle.getPosition().x + 10, circle.getPosition().y - 10 , 8);
+    projectile.get()->body->ApplyForceToCenter(b2Vec2(multiply * vel.x, multiply * vel.y), true);
+    //projectile.get()->body->SetLinearVelocity(vel);
+    projectiles.push_back(projectile);
+    
+    circle.body->SetLinearVelocity(b2Vec2(0,0));
 }
--- BaseBox2D/src/ofApp.h
+++ BaseBox2D/src/ofApp.h
@@ -4,30 +4,37 @@
 #include "ofxBox2d.h"
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-
-		ofxBox2d                                  box2d;   // the box2d world
-		ofxBox2dCircle                            anchor;  // fixed 
-		ofxBox2dCircle                            anchor2;  // fixed anchor
-		ofxBox2dCircle							  circle; // circulo
-		ofxBox2dCircle							  projectile; // circulo
-		vector      <shared_ptr<ofxBox2dJoint> >  joints;  // joints
-		
-		bool drag = false; // if the point was draged
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    void addProjectile();
+    
+    ofxBox2d                                  box2d;   // the box2d world
+    ofxBox2dCircle                            anchor;  // fixed
+    ofxBox2dCircle                            anchor2;  // fixed anchor
+    ofxBox2dCircle							  circle; // circulo
+    ofxBox2dRect                              ground; //chao
+    ofxBox2dRect                              ground2; //chao
+    ofxBox2dRect                              wall1; //chao
+    ofxBox2dRect                              wall2; //chao
+    vector      <shared_ptr<ofxBox2dPolygon> > targets; // circulos
+    vector      <shared_ptr<ofxBox2dCircle> > projectiles; // circulos
+    vector      <shared_ptr<ofxBox2dJoint> >  joints;  // joints
+    
+    
+    bool drag = false; // if the point was draged
 };
