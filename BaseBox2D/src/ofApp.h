51daa9110f0867f26bd6a8a21ea8008a9b04515e
André Bovendorp <Andr Bovendorp>
2016-09-27 00:19:56 -0300

    Catapulta quase pronta

--- BaseBox2D/src/ofApp.h
+++ BaseBox2D/src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxBox2d.h"
 
 class ofApp : public ofBaseApp{
 
@@ -20,5 +21,13 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+		ofxBox2d                                  box2d;   // the box2d world
+		ofxBox2dCircle                            anchor;  // fixed 
+		ofxBox2dCircle                            anchor2;  // fixed anchor
+		ofxBox2dCircle							  circle; // circulo
+		ofxBox2dCircle							  projectile; // circulo
+		vector      <shared_ptr<ofxBox2dJoint> >  joints;  // joints
 		
+		bool drag = false; // if the point was draged
 };
